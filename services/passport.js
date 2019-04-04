63924d8843fafda5d021784e8f3cfced286b42b3
sssinghsyr <sssinghsyr@users.noreply.github.com>
2019-04-03 20:48:28 -0700

    updated promise with async await syntax

--- services/passport.js
+++ services/passport.js
@@ -22,23 +22,21 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          console.log(
-            "Existing User has logged in with emailId: ",
-            existingUser.emailId
-          );
-          done(null, existingUser);
-        } else {
-          new User({
-            googleId: profile.id,
-            emailId: profile._json.email
-          })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        console.log(
+          "Existing User has logged in with emailId: ",
+          existingUser.emailId
+        );
+        done(null, existingUser);
+      } else {
+        const user = await new User({
+          googleId: profile.id,
+          emailId: profile._json.email
+        }).save();
+        done(null, user);
+      }
     }
   )
 );
