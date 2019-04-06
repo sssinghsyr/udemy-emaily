3ae53a859346df2d62b04ba65ccbd538e60f96de
sssinghsyr <sssinghsyr@users.noreply.github.com>
2019-04-06 12:56:03 -0700

    Rendering Header componnent based upon login state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderLoginContent() {
+    //console.log(this.props.auth);
+    switch (this.props.auth) {
+      case null:
+        return "Loading...";
+      case false:
+        return "Login with Google";
+      default:
+        return "Hello " + this.props.auth.emailId;
+    }
+  }
+
   render() {
+    //console.log(this.props);
     return (
       //<div>Header</div>
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderLoginContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
